553473aa2130fdfca3b0d56550a7d215073d9ab9
yujin <claire9585@sookmyung.ac.kr>
2021-02-14 00:44:24 +0900

    Saving the User name

---
+++ greeting.js
@@ -0,0 +1,44 @@
+const form = document.querySelector(".js-form"),
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+  SHOWING_CN = "showing";
+
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault(); // form에서 enter 눌러도 사라지지 않음
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName() {
+  form.classList.add(SHOWING_CN);
+  form.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(text) {
+  form.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello ${text}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    // 유저 없을 때
+    askForName();
+  } else {
+    // 유저 있을 때
+    paintGreeting(currentUser);
+  }
+}
+
+function init() {
+  loadName();
+}
+init();
